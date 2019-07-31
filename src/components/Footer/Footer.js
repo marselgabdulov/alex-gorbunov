477d3a001ad66d6fcf11c3db01905d13bdcab1ea
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-07-31 07:31:53 +0300

    Create basic responsive Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,108 @@
 import React from "react"
 import "./Footer.scss"
+import { Link } from "gatsby"
+import Logo from "../Logo/Logo"
 
 function Footer(props) {
   return (
-    <footer className="footer">
-      <div className="footer__copy">
-        © {new Date().getFullYear()}, Алексей Горбунов
+    <footer className="footer" id="contacts">
+      <div className="footer-logo">
+        <Logo />
       </div>
-      <div className="footer__developer">
-        Разработан{" "}
-        <a
-          href="https://marselgabdulov.github.io/personal/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MG
-        </a>
+      <div className="footer-contacts">
+        <div className="footer__phone">
+          <span className="footer__title">Позвоните</span>
+          <a href="tel:+79166228889">+7 916 622 88 89</a>
+        </div>
+
+        <div className="footer__email">
+          <span className="footer__title">Напишите</span>
+          <a href="mailto:alex.gorbunov@gmail.com">alex.gorbunov@gmail.com</a>
+        </div>
+      </div>
+      <div className="footer-social">
+        <span className="footer__title">Социальные сети:</span>
+        <div className="footer-social__link">
+          <Link to="vk.com" target="_blank" rel="noopener noreferrer">
+            VK
+          </Link>
+        </div>
+        <div className="footer-social__link">
+          <Link
+            to="https://www.facebook.com/alex.gorbunov.969/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </Link>
+        </div>
+        <div className="footer-social__link">
+          <Link
+            to="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </Link>
+        </div>
+        <div className="footer-social__link">
+          <Link
+            to="https://www.youtube.com/watch?v=xkAQ6bhpwYw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Youtube
+          </Link>
+        </div>
+        <div className="footer-social__link">
+          <Link
+            to="https://vimeo.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vimeo
+          </Link>
+        </div>
+      </div>
+      <div className="footer-menu">
+        <span className="footer__title">Меню:</span>
+        <div className="footer-menu__link">
+          <Link to="/">свадьбы</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">корпоративы</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">детские</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">частные</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">дни рождения</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">городские</Link>
+        </div>
+        <div className="footer-menu__link">
+          <Link to="/">промоакции</Link>
+        </div>
+      </div>
+
+      <div className="footer-subfooter">
+        <div className="footer__copy">
+          © {new Date().getFullYear()}, Алексей Горбунов
+        </div>
+        <div className="footer__developer">
+          Разработан{" "}
+          <a
+            href="https://marselgabdulov.github.io/personal/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MG
+          </a>
+        </div>
       </div>
     </footer>
   )
