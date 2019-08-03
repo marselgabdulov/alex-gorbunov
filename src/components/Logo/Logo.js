63a0bfc1b1251b4e220a4a95341d028a70b654e9
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-08-03 10:25:37 +0300

    Simplify logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,20 +4,16 @@ import { Link } from "gatsby"
 
 function Logo() {
   return (
-    <div className="logo-wrapper">
-      <div
-        className="logo"
-        data-aos="fade"
-        data-aos-delay="100"
-        data-aos-duration="500"
-        data-aos-once="true"
-      >
-        <div className="logo__link">
-          <Link to="/" title="вернуться на главную">
-            Алексей Горбунов
-          </Link>
-        </div>
-      </div>
+    <div
+      className="logo"
+      data-aos="fade"
+      data-aos-delay="100"
+      data-aos-duration="500"
+      data-aos-once="true"
+    >
+      <Link to="/" title="вернуться на главную">
+        Алексей Горбунов
+      </Link>
     </div>
   )
 }
