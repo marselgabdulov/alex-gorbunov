60bf636936cec804338a3cfc23509f82bfcc44e5
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-08-03 09:33:19 +0300

    Simplify NavPannel

--- src/components/NavPannel/NavPannel.js
+++ src/components/NavPannel/NavPannel.js
@@ -20,11 +20,34 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
+const links = [
+  {
+    to: "/",
+    name: "Главная",
+  },
+  {
+    to: "/wedding",
+    name: "Свадьбы",
+  },
+  {
+    to: "/corporate",
+    name: "Корпоративы",
+  },
+  {
+    to: "/celebration",
+    name: "Праздники",
+  },
+  {
+    to: "/reviews",
+    name: "Отзывы",
+  },
+]
+
 function NavPannel({ toggleNavPannel, isNavPannelVisible }) {
   return (
     <>
       <div className="nav-pannel">
-        <div className={isNavPannelVisible ? "nav-opened" : "nav-closed"}>
+        <div className={!isNavPannelVisible ? "nav-opened" : "nav-closed"}>
           <button
             className="nav-pannel__close-button"
             onClick={() => toggleNavPannel()}
@@ -36,180 +59,85 @@ function NavPannel({ toggleNavPannel, isNavPannelVisible }) {
               <div className="three"></div>
             </div>
           </button>
-          <div className="nav-pannel__wrapper">
-            <div className="nav-pannel__left">
-              <div className="nav-pannel__left--wrapper">
-                <div className="nav-pannel__logo">
-                  <div className="nav-pannel__logo--name">
-                    Алексей Горбунов.
-                  </div>
-                  <div className="nav-pannel__logo--text">Ведущий. Москва.</div>
-                </div>
-                <div className="nav-pannel__phone">
-                  <a href="tel:+79166228889">+7 916 622 88 89</a>
-                </div>
-                <div className="nav-pannel__phone--mobile">
-                  <a href="tel:+79166228889">
-                    <Phone />
-                  </a>
-                </div>
-                <div className="nav-pannel__email">
-                  <a href="mailto:alex.gorbunov@gmail.com">
-                    alex.gorbunov@gmail.com
-                  </a>
-                </div>
-                <div className="nav-pannel__social">
-                  <div className="nav-pannel__social-icon">
-                    <a
-                      href="vk.com"
-                      title="в контакте"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Vk />
-                    </a>
-                  </div>
-                  <div className="nav-pannel__social-icon">
-                    <a
-                      href="https://www.facebook.com/alex.gorbunov.969"
-                      title="facebook"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Facebook />
-                    </a>
-                  </div>
-                  <div className="nav-pannel__social-icon">
-                    <a
-                      href="https://www.instagram.com/"
-                      title="instagram"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Instagram />
-                    </a>
-                  </div>
-                  <div className="nav-pannel__social-icon">
-                    <a
-                      href="https://www.youtube.com/watch?v=xkAQ6bhpwYw"
-                      title="youtube"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Youtube />
-                    </a>
-                  </div>
-                  <div className="nav-pannel__social-icon">
-                    <a
-                      href="https://vimeo.com/"
-                      title="vimeo"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Vimeo />
-                    </a>
-                  </div>
-                </div>
-                <div className="nav-pannel__creator">
-                  Разработан{" "}
-                  <a
-                    href="https://marselgabdulov.github.io/personal/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    MG
-                  </a>
-                </div>
-                <div className="nav-pannel__copy">
-                  <span>&copy; 2019. Алексей Горбунов. &nbsp;</span>
-                  <span>Все права защищены.</span>
-                </div>
+
+          <div className="nav-pannel__menu">
+            {links.map((item, index) => (
+              <div className="menu__item" key={index}>
+                <TransitionLink
+                  to={item.to}
+                  onClick={() => toggleNavPannel()}
+                  exit={{
+                    length: 0.5,
+                  }}
+                  entry={{
+                    delay: 1,
+                  }}
+                >
+                  {item.name}
+                </TransitionLink>
               </div>
+            ))}
+
+            <div className="nav-pannel__phone">
+              <a href="tel:+79166228889">+7 916 622 88 89</a>
             </div>
-            <div className="nav-pannel__wrapper--right">
-              <div className="nav-pannel__right-wrapper">
-                <TransitionLink
-                  title="Главная"
-                  to="/"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
-                >
-                  Главная
-                </TransitionLink>
 
-                <TransitionLink
-                  activeStyle={{ color: "#e82a6e" }}
-                  to="/reviews"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
-                >
-                  Отзывы
-                </TransitionLink>
+            <div className="nav-pannel__email">
+              <a href="mailto:alex.gorbunov@gmail.com">
+                alex.gorbunov@gmail.com
+              </a>
+            </div>
 
-                <TransitionLink
-                  activeStyle={{ color: "#e82a6e" }}
-                  to="/services#wedding"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
+            <div className="nav-pannel__social-links">
+              <div className="nav-pannel__social-icon">
+                <a
+                  href="vk.com"
+                  title="в контакте"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Свадьбы
-                </TransitionLink>
-
-                <TransitionLink
-                  activeStyle={{ color: "#e82a6e" }}
-                  to="/services#corporate"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
+                  <Vk />
+                </a>
+              </div>
+              <div className="nav-pannel__social-icon">
+                <a
+                  href="https://www.facebook.com/alex.gorbunov.969"
+                  title="facebook"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Корпоративы
-                </TransitionLink>
-
-                <TransitionLink
-                  activeStyle={{ color: "#e82a6e" }}
-                  to="/services#kids"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
+                  <Facebook />
+                </a>
+              </div>
+              <div className="nav-pannel__social-icon">
+                <a
+                  href="https://www.instagram.com/"
+                  title="instagram"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Детские праздники
-                </TransitionLink>
-                <TransitionLink
-                  activeStyle={{ color: "#e82a6e" }}
-                  to="/services"
-                  onClick={() => toggleNavPannel()}
-                  exit={{
-                    length: 0.5,
-                  }}
-                  entry={{
-                    delay: 1,
-                  }}
+                  <Instagram />
+                </a>
+              </div>
+              <div className="nav-pannel__social-icon">
+                <a
+                  href="https://www.youtube.com/watch?v=xkAQ6bhpwYw"
+                  title="youtube"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  все услуги
-                </TransitionLink>
+                  <Youtube />
+                </a>
+              </div>
+              <div className="nav-pannel__social-icon">
+                <a
+                  href="https://vimeo.com/"
+                  title="vimeo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Vimeo />
+                </a>
               </div>
             </div>
           </div>
