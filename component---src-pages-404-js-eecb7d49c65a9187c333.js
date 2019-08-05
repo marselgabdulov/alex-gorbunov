476e1afb9d74f8b45475218c0780669c090d5b9f
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-08-05 15:13:15 +0300

    Updates

--- component---src-pages-404-js-eecb7d49c65a9187c333.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{244:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(263),o=n(262);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-eecb7d49c65a9187c333.js.map
