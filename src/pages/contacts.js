cb1d50b1b350dcf148e0b27ebc8a34b5254f854a
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-08-09 17:22:03 +0300

    Add basic content to contacts page

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -8,7 +8,74 @@ function ContactsPage(props) {
     <Layout>
       <SEO title="Контакты" />
       <div className="contacts-page">
-        <h1>В разработке</h1>
+        <div className="contacts">
+          <div
+            className="phone"
+            data-aos="fade"
+            data-aos-delay="400"
+            data-aos-duration="1200"
+            data-aos-once="true"
+            data-aos-anchor-placement="center-bottom"
+          >
+            <h2>телефон</h2>
+            <a href="tel:+79856446489">+7 985 644 64 89</a>
+          </div>
+          <div
+            className="email"
+            data-aos="fade"
+            data-aos-delay="450"
+            data-aos-duration="1200"
+            data-aos-once="true"
+            data-aos-anchor-placement="center-bottom"
+          >
+            <h2>email</h2>
+            <a href="mailto:gorbunov_show@mail.ru">gorbunov_show@mail.ru</a>
+          </div>
+        </div>
+        <div className="social">
+          <div className="facebook">
+            <a
+              href="https://www.facebook.com/alex.gorbunov.969"
+              target="_blank"
+              rel="noopener noreferrer"
+              data-aos="fade"
+              data-aos-delay="250"
+              data-aos-duration="1200"
+              data-aos-once="true"
+              data-aos-anchor-placement="center-bottom"
+            >
+              Facebook
+            </a>
+          </div>
+          <div className="instagram">
+            <a
+              href="https://instagram.com/alexgrant_show?igshid=1loy3l4uz5erh"
+              target="_blank"
+              rel="noopener noreferrer"
+              data-aos="fade"
+              data-aos-delay="300"
+              data-aos-duration="1200"
+              data-aos-once="true"
+              data-aos-anchor-placement="center-bottom"
+            >
+              Instagram
+            </a>
+          </div>
+          <div className="vk">
+            <a
+              href="https://vk.com/alexgrantshow"
+              target="_blank"
+              rel="noopener noreferrer"
+              data-aos="fade"
+              data-aos-delay="350"
+              data-aos-duration="1200"
+              data-aos-once="true"
+              data-aos-anchor-placement="center-bottom"
+            >
+              Vk
+            </a>
+          </div>
+        </div>
       </div>
     </Layout>
   )
