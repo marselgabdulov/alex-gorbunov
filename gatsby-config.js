4aa2408c907f8a31a166435d81577b7018c5b51e
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-08-20 21:39:38 +0300

    Play with robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       options: {
         host: "https://alexeygorbunov.com",
         sitemap: "https://alexeygorbunov/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
+        policy: [{ userAgent: "*", disallow: "" }],
       },
     },
     `gatsby-plugin-sass`,
